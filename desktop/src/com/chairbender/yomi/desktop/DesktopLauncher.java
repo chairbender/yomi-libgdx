f20ad6a42b639f2a6c9108a005e6882fef36c9b1
Kyle Hipke <kwhipke1@gmail.com>
2014-07-14 20:56:52 -0700

    Initial commit

---
+++ desktop/src/com/chairbender/yomi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chairbender.yomi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chairbender.yomi.YomiLibgdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YomiLibgdx(), config);
+	}
+}
