171bf80a24d882803a0773a87db2fd74d24fbcd8
Kyle Hipke <kwhipke1@gmail.com>
2014-07-18 11:36:38 -0700

    implemented attack starburst and block damage icon. Next step is to change the move info to match yomi cards

--- core/src/com/chairbender/yomi/ui/CardGroup.java
+++ core/src/com/chairbender/yomi/ui/CardGroup.java
@@ -2,19 +2,12 @@ package com.chairbender.yomi.ui;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.RotateByAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.chairbender.yomi.api.card.Card;
@@ -30,10 +23,10 @@ public class CardGroup extends Group {
     private Card card;
 
     private Image cardImage;
-    private float scale = 2f;
+    private float scale = 2.2f;
 
-    private MoveActor topMove;
-    private MoveActor bottomMove;
+    private MoveInfoGroup topMove;
+    private MoveInfoGroup bottomMove;
 
     private float rotation = 0;
 
@@ -53,8 +46,8 @@ public class CardGroup extends Group {
         this.cardImage = new Image(cardTexture);
         cardImage.setBounds(0,0,cardTexture.getWidth()*scale,cardTexture.getHeight()*scale);
         //add the move info
-        topMove = new MoveActor(card.topMoveInfo());
-        bottomMove = new MoveActor(card.bottomMoveInfo());
+        topMove = new MoveInfoGroup(card.topMoveInfo(),this);
+        bottomMove = new MoveInfoGroup(card.bottomMoveInfo(),this);
         addActor(cardImage);
         addActor(topMove);
         addActor(bottomMove);
@@ -76,9 +69,8 @@ public class CardGroup extends Group {
      * position everything based on this group's coordinates and bounds
      */
     private void positionEverything() {
-
-        topMove.setPosition(25,cardImage.getHeight() - topMove.getHeight());
-        bottomMove.setPosition(25,cardImage.getHeight() - bottomMove.getHeight());
+        topMove.setPosition(27,cardImage.getHeight() - topMove.getHeight());
+        bottomMove.setPosition(27,cardImage.getHeight() - bottomMove.getHeight());
         //set origins to this card's middle point
         this.setOrigin(getWidth()/2,getHeight()/2);
         topMove.setOrigin(getWidth()/2 - topMove.getX(), getHeight()/2 - topMove.getY());
@@ -96,9 +88,6 @@ public class CardGroup extends Group {
      */
     public void rotate() {
         this.addAction(Actions.rotateBy(180,0.5f, Interpolation.pow4));
-//        topMove.addAction(Actions.rotateBy(180,0.5f, Interpolation.pow4));
-//        bottomMove.addAction(Actions.rotateBy(180,0.5f, Interpolation.pow4));
-//        cardImage.addAction(Actions.rotateBy(180,0.5f, Interpolation.pow4));
     }
 
     @Override
@@ -162,23 +151,51 @@ public class CardGroup extends Group {
 }
 
 /**
- * actor for drawing move info
+ * group for drawing move info
  */
-class MoveActor extends Group {
+class MoveInfoGroup extends Group {
     private final MoveInfo move;
 
+    private static final float scale = 0.8f;
+    private static final float largeScale = 1.2f;
+    private static final float iconScale = 0.8f;
+    private static final float blockDamageIconScale = 0.4f;
     private Label moveInfoLabel;
+    private Label moveTypeLabel;
+    private Image moveTypeIcon;
+    private Label damageLabel;
 
-    public MoveActor(MoveInfo move) {
+    private Label blockDamageLabel;
+    private Image blockDamageImage;
+
+
+
+    public MoveInfoGroup(MoveInfo move,CardGroup parent) {
         this.move = move;
 
         String moveInfoString = "";
+        String moveTypeString = "";
+        String moveTypeIconFile = "";
+        String damageString = "";
+        String blockDamageString = null;
+        Color typeColor;
         if (move instanceof OffensiveMoveInfo) {
             OffensiveMoveInfo offense = (OffensiveMoveInfo) move;
             if (offense.getMoveType().equals(MoveType.ATTACK)) {
-                moveInfoString += "Attack\n";
+                moveTypeString += "Attack\n";
+                moveTypeIconFile = "icons/attack.png";
+                typeColor = Color.RED;
             } else {
-                moveInfoString += "Throw\n";
+                moveTypeString += "Throw\n";
+                typeColor = Color.BLACK;
+                moveTypeIconFile = "icons/throw.png";
+            }
+            if (offense.getBlockDamage() > 0) {
+                blockDamageString = offense.getBlockDamage() + "";
+            }
+            damageString = offense.getDamage() + "";
+            if (offense.getPumpDamage() > 0) {
+                damageString += "+" + offense.getPumpDamage();
             }
             moveInfoString += "Speed: " + offense.getSpeed().toString() + "\n";
             moveInfoString += "Combo Points: " + offense.getComboPoints() + "\n";
@@ -198,18 +215,64 @@ class MoveActor extends Group {
             }
         } else {
             if (move.getMoveType().equals(MoveType.BLOCK)) {
-                moveInfoString += "Block";
+                moveTypeString += "Block";
+                typeColor = Color.BLUE;
+                moveTypeIconFile = "icons/block.png";
             } else {
-                moveInfoString += "Dodge";
+                moveTypeString += "Dodge";
+                typeColor = Color.PURPLE;
+                moveTypeIconFile = "icons/dodge.png";
             }
         }
 
+
+
+        moveTypeLabel   = new Label(moveTypeString, new Label.LabelStyle(new BitmapFont(),typeColor));
+
+
         moveInfoLabel = new Label(moveInfoString, new Label.LabelStyle(new BitmapFont(),Color.BLACK));
+        moveTypeLabel.setPosition(getX(),moveInfoLabel.getY() + moveInfoLabel.getHeight());
+        moveInfoLabel.setFontScale(scale);
+        moveTypeLabel.setFontScale(largeScale);
+
+        Texture moveIcon = new Texture(moveTypeIconFile);
+        moveTypeIcon = new Image(moveIcon);
+        damageLabel = new Label(damageString, new Label.LabelStyle(new BitmapFont(),Color.WHITE));
+        positionDamageIcon(moveTypeIcon, damageLabel,moveTypeLabel.getX() + 88,moveTypeLabel.getY() - 20, iconScale);
+
+
         addActor(moveInfoLabel);
+        addActor(moveTypeLabel);
+        addActor(moveTypeIcon);
+        addActor(damageLabel);
+
+        //handle block damage
+        if (blockDamageString != null) {
+            Texture blockIcon = new Texture("icons/block.png");
+            blockDamageImage = new Image(blockIcon);
+            blockDamageLabel = new Label(blockDamageString, new Label.LabelStyle(new BitmapFont(),Color.WHITE));
+            blockDamageLabel.setFontScale(.8f);
+            positionDamageIcon(blockDamageImage, blockDamageLabel,
+                    moveTypeIcon.getX() + moveTypeIcon.getWidth() - blockDamageImage.getWidth()/2,
+                    moveTypeIcon.getY() - (blockDamageImage.getHeight()*blockDamageIconScale)/2,blockDamageIconScale);
+
+            addActor(blockDamageImage);
+            addActor(blockDamageLabel);
+
+        }
+    }
+
+    private void positionDamageIcon(Image icon, Label damage,float x, float y, float iconScale) {
+        //position relative to move label
+        icon.setPosition(x,y);
+        icon.setWidth(icon.getWidth()*iconScale);
+        icon.setHeight(icon.getHeight() * iconScale);
+        damage.setPosition(icon.getX(),icon.getY());
+        damage.setCenterPosition(icon.getCenterX(),icon.getCenterY());
     }
 
     @Override
     public float getHeight() {
-        return moveInfoLabel.getHeight();
+        return moveInfoLabel.getHeight() + moveTypeLabel.getHeight();
     }
 }
